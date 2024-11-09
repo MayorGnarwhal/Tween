dffebcab42cfc19f09cd8c9a74547f1df0a8a784
Harris Fort <46070329+MayorGnarwhal@users.noreply.github.com>
2024-11-09 14:08:53 -0500

    Added typechecking

--- Tween.lua
+++ Tween.lua
@@ -4,79 +4,82 @@ Tween.__index = Tween
 --// Dependencies
 local TweenService = game:GetService("TweenService")
 
+--// Variables
+export type TweenClass = typeof(setmetatable({} :: {
+	Instance: instance,
+	TweenInfo: TweenInfo,
+	Tweens: {Tween},
+	DummyValues: {BaseValue},
+	Completed: RBXScriptSignal,
+}, Tween))
+
 
 --// Helper functions
---[[
-	Get a ValueBase for the type of the given variable
-]]
-local function GetValueBase(var: any)
-	local varType = typeof(var)
+local function GetValueBase(var: any): ValueBase
+	local typeofVar = typeof(var)
 	local valueBase
 
-	if varType == "boolean" then
+	if typeofVar == "boolean" then
 		valueBase = Instance.new("BoolValue")
+	elseif typeofVar == "Instance" then
+		valueBase = Instance.new("ObjectValue")
 	else
-		valueBase = Instance.new(varType:gsub("^%l", string.upper) .. "Value")
+		valueBase = Instance.new(`{typeofVar:gsub("^%l", string.upper)}Value`)
 	end
 
 	valueBase.Value = var
+
 	return valueBase
 end
 
-local function GetProperty(instance, property)
-	if property == "CFrame" then
-		if instance:IsA("Model") or instance:IsA("BasePart") then
+local function GetProperty(instance: Instance, property: string): any
+	if instance:IsA("Model") then
+		if property == "CFrame" then
 			return instance:GetPivot()
-		else
-			return instance.CFrame
+		elseif property == "Size" or property == "Scale" then
+			return instance:GetScale()
 		end
-	elseif property == "Size" or property == "Scale" then
-		return instance:IsA("Model") and instance:GetScale() or instance.Size
-	elseif instance:GetAttribute(property) ~= nil then
+	end
+
+	if instance:GetAttribute(property) ~= nil then
 		return instance:GetAttribute(property)
 	else
 		return instance[property]
 	end
 end
 
-local function GetSetFunction(instance, property)
-	if property == "CFrame" then
-		if instance:IsA("Model") or instance:IsA("BasePart") then
+local function GetSetFunction(instance: Instance, property: string): (instance: Instance, value: any) -> nil
+	if instance:IsA("Model") then
+		if property == "CFrame" then
 			return instance.PivotTo
-		else
-			return function(instance, value)
-				instance.CFrame = value
-			end
-		end
-	elseif property == "Size" or property == "Scale" then
-		if instance:IsA("Model") then
+		elseif property == "Size" or property == "Scale" then
 			return instance.ScaleTo
-		else
-			return function(instance, value)
-				instance.Size = value
-			end
 		end
-	elseif instance:GetAttribute(property) ~= nil then
+	end
+
+	if instance:GetAttribute(property) ~= nil then
 		return function(instance, value)
 			instance:SetAttribute(property, value)
 		end
-	else
-		return function(instance, value)
-			instance[property] = value
-		end
+	end
+
+	assert(instance[property])
+	return function(instance, value)
+		instance[property] = value
 	end
 end
 
-local function CreateDummyTween(instance: Instance, tweenInfo: TweenInfo, property: string, targetValue: any, byDiff: boolean)
+local function CreateDummyTween(instance: Instance, tweenInfo: TweenInfo, property: string, targetValue: any, byDelta: boolean): (Tween, ValueBase)
 	local currentValue = GetProperty(instance, property)
 	local setFunction = GetSetFunction(instance, property)
-	if currentValue == nil then return end
 
 	local dummyValue = GetValueBase(currentValue)
-	local lastValue = dummyValue.Value
+	local lastValue = currentValue
+
 	dummyValue.Changed:Connect(function()
-		if byDiff then
-			setFunction(instance, GetProperty(instance, property) + (dummyValue.Value - lastValue))
+		if byDelta then
+			local delta = (dummyValue.Value - lastValue)
+			setFunction(instance, GetProperty(instance, property) + delta)
 			lastValue = dummyValue.Value
 		else
 			setFunction(instance, dummyValue.Value)
@@ -88,132 +91,113 @@ local function CreateDummyTween(instance: Instance, tweenInfo: TweenInfo, proper
 	return dummyTween, dummyValue
 end
 
+local function CreateTween(instance: Instance, tweenInfo: TweenInfo, propertyTable: {}, byDelta: boolean): TweenClass
+	local self = setmetatable({}, Tween)
+	self.Instance = instance
+	self.TweenInfo = tweenInfo
+	self.Tweens = {}
+	self.DummyValues = {}
 
---// Constructors
-local function ConstructTween(instance: Instance, tweenInfo: TweenInfo, propertyTable: table, byDiff: boolean)
-	local self = setmetatable({
-		Instance = instance;
-		TweenInfo = tweenInfo;
-		Tweens = {};
-		DummyValues = {};
-	}, Tween)
+	propertyTable = table.clone(propertyTable)
 
-	-- tweening a model's cframe
-	if instance:IsA("Model") and (propertyTable.CFrame or propertyTable.Value) then
-		local baseCF = propertyTable.CFrame or propertyTable.Value
-		local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, "CFrame", baseCF, byDiff)
-		propertyTable.CFrame = nil
-		propertyTable.Value = nil
+	if instance:IsA("Model") then
+		if propertyTable.CFrame ~= nil then
+			local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, "CFrame", propertyTable.CFrame, byDelta)
+			table.insert(self.Tweens, dummyTween)
+			table.insert(self.DummyValues, dummyValue)
+			propertyTable["CFrame"] = nil
+		end
 
-		table.insert(self.Tweens, dummyTween)
-		table.insert(self.DummyValues, dummyValue)
+		if propertyTable.Size ~= nil or propertyTable.Scale ~= nil then
+			local currentSize = propertyTable.Size or propertyTable.Scale
+			local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, "Size", currentSize, byDelta)
+			table.insert(self.Tweens, dummyTween)
+			table.insert(self.DummyValues, dummyValue)
+			propertyTable["Size"] = nil
+			propertyTable["Scale"] = nil
+		end
 	end
 
-	if instance:IsA("Model") and (propertyTable.Size or propertyTable.Scale) then
-		local baseScale = propertyTable.Size or propertyTable.Scale
-		local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, "Size", baseScale, byDiff)
-		propertyTable.Size = nil
-		propertyTable.Scale = nil
-
-		table.insert(self.Tweens, dummyTween)
-		table.insert(self.DummyValues, dummyValue)
-	end
-
-	-- tweening an attribute
 	for attribute, value in pairs(propertyTable) do
 		if instance:GetAttribute(attribute) ~= nil then
-			local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, attribute, value, byDiff)
+			local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, attribute, value, byDelta)
+			table.insert(self.Tweens, dummyTween)
+			table.insert(self.DummyValues, dummyValue)
 			propertyTable[attribute] = nil
-
-			table.insert(self.Tweens, dummyTween)
-			table.insert(self.DummyValues, dummyValue)
 		end
 	end
 
-	if byDiff and next(propertyTable) ~= nil then
-		for property, value in pairs(propertyTable) do
-			local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, property, value, byDiff)
-			table.insert(self.Tweens, dummyTween)
-			table.insert(self.DummyValues, dummyValue)
-		end
-	else
-		local tween = TweenService:Create(instance, tweenInfo, propertyTable)
-		table.insert(self.Tweens, 1, tween)
-	end
-
-	-- attach Completed event
-	self.Completed = self.Tweens[1].Completed
-
-	return self	
-end
-
---[[
-	Create a standard tween object
-]]
-function Tween:Create(instance: Instance, tweenInfo: TweenInfo, propertyTable: table)
-	return ConstructTween(instance, tweenInfo, propertyTable, false)
-end
-
---[[
-	Create a Tween who's target is the current value plus the given value
-	Uses all Dummy Tweens and increments by difference
-		Allows for multiple tweens affecting same property of an instance
-]]
-function Tween:CreateDiff(instance: Instance, tweenInfo: TweenInfo, propertyTable: table)
-	for attribute, value in pairs(propertyTable) do
-		local currentValue = GetProperty(instance, attribute)
-		if typeof(currentValue) == "CFrame" and typeof(value) == "CFrame" then
-			propertyTable[attribute] = currentValue * value
+	if next(propertyTable) ~= nil then
+		if byDelta then
+			for property, value in pairs(propertyTable) do
+				local dummyTween, dummyValue = CreateDummyTween(instance, tweenInfo, property, value, byDelta)
+				table.insert(self.Tweens, dummyTween)
+				table.insert(self.DummyValues, dummyValue)
+			end
 		else
-			propertyTable[attribute] = currentValue + value
+			local tween = TweenService:Create(instance, tweenInfo, propertyTable)
+			table.insert(self.Tweens, 1, tween)
 		end
 	end
 
-	return ConstructTween(instance, tweenInfo, propertyTable, true)
-end
-
---[[
-	Create a dummy tween between two values, and connect a closure on Changed event
-	onChange closure recieves the current value, and the previous value of the dummy ValueBase
-]]
-function Tween:Connect(first: any, last: any, tweenInfo: TweenInfo, onChange: Function)
-	assert(typeof(first) == typeof(last), "Attempting to tween from " .. typeof(first) .. " to " .. typeof(last) .. ".")
-
-	local valueBase = GetValueBase(first)
-	valueBase.Value = first
-
-	local lastValue = first
-	valueBase.Changed:Connect(function()
-		onChange(valueBase.Value, lastValue)
-		lastValue = valueBase.Value
-	end)
-
-	return ConstructTween(valueBase, tweenInfo, {Value = last}, false)
-end
-
-
---// TweenService GetValue Method
-function Tween:GetValue(alpha: number, easingStyle: Enum.EasingStyle, easingDirection: Enum.EasingDirection)
-	return TweenService:GetValue(alpha, easingStyle, easingDirection)
-end
-
-
---// Default Tween Methods
-function Tween:Play()
-	if self:PlaybackState() == Enum.PlaybackState.Begin then
-		self:andThen(function()
-			self:Destroy()
-		end)
-	end
-
-	for i, tween in pairs(self.Tweens) do
-		tween:Play()
-	end
+	self.Completed = self.Tweens[1].Completed
 
 	return self
 end
 
-function Tween:Pause()
+
+--// Constructors
+function Tween:Create(instance: Instance, tweenInfo: TweenInfo, propertyTable: {}): TweenClass
+	return CreateTween(instance, tweenInfo, propertyTable, false)
+end
+
+function Tween:CreateByDelta(instance: Instance, tweenInfo: TweenInfo, propertyTable: {}): TweenClass
+	local deltaPropertyTable = {}
+
+	for property, value in pairs(propertyTable) do
+		local currentValue = GetProperty(instance, property)
+		if typeof(value) == "CFrame" then
+			deltaPropertyTable[property] = currentValue * value
+		else
+			deltaPropertyTable[property] = currentValue + value
+		end
+	end
+
+	return CreateTween(instance, tweenInfo, deltaPropertyTable, true)
+end
+
+function Tween:Connect<T>(first: T, last: T, tweenInfo: TweenInfo, update: (value: T, lastValue: T) -> nil): TweenClass
+	assert(typeof(first) == typeof(last),
+		`Attempted to tween between value types ({typeof(first)} to {typeof(last)})`
+	)
+
+	local valueBase = GetValueBase(first)
+	local lastValue = first
+
+	valueBase.Changed:Connect(function()
+		update(valueBase.Value, lastValue)
+		lastValue = valueBase.Value
+	end)
+
+	return CreateTween(valueBase, tweenInfo, {Value = last}, false)
+end
+
+--// Methods
+function Tween.Play(self: TweenClass, doNotCleanup: boolean?): TweenClass
+	if not doNotCleanup and self:PlaybackState() == Enum.PlaybackState.Begin then
+		self.Completed:Once(function()
+			self:Destroy()
+		end)
+	end
+	
+	for i, tween in pairs(self.Tweens) do
+		tween:Play()
+	end
+	
+	return self
+end
+
+function Tween.Pause(self: TweenClass): TweenClass
 	for i, tween in pairs(self.Tweens) do
 		tween:Pause()
 	end
@@ -221,7 +205,7 @@ function Tween:Pause()
 	return self
 end
 
-function Tween:Cancel()
+function Tween.Cancel(self: TweenClass): TweenClass
 	for i, tween in pairs(self.Tweens) do
 		tween:Cancel()
 	end
@@ -229,18 +213,11 @@ function Tween:Cancel()
 	return self
 end
 
-
---// Custom Tween Methods
-function Tween:PlaybackState()
+function Tween.PlaybackState(self: TweenClass): Enum.PlaybackState
 	return self.Tweens[1].PlaybackState
 end
 
---[[
-	Wait until Tween has completed playing or is stopped with :Cancel()
-	If a durationMult is given, then yield for a duration relative to the Tweens's length
-	Returns the Tween's current PlaybackState
-]]
-function Tween:Yield(durationMult: number?)
+function Tween.Yield(self: TweenClass, durationMult: number?): Enum.PlaybackState
 	if durationMult then
 		task.wait(self.TweenInfo.Time * durationMult)
 	else
@@ -252,25 +229,16 @@ function Tween:Yield(durationMult: number?)
 	return self:PlaybackState()
 end
 
---[[
-	Asynchronously call a function when the Tween is completed or stopped with :Cancel()
-]]
-function Tween:andThen(callback: Function)
-	task.spawn(function()
-		local playbackState = self:Yield()
-		callback(playbackState)
-	end)
-
-	return self
+function Tween.andThen(self: TweenClass, callback: () -> Enum.PlaybackState)
+	self.Completed:Connect(callback)
 end
 
---[[
-	Clean up resources used by custom Tween objects
-	Called automatically when tween is played and has been completed or canceled
-]]
-function Tween:Destroy()
+function Tween.Destroy(self: TweenClass)
 	for i, value in pairs(self.DummyValues) do
 		value:Destroy()
+	end
+	for i, tween in pairs(self.Tweens) do
+		tween:Destroy()
 	end
 end
 
